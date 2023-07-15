a1823a5ea58197c0d0378b2f8fce9624f208d89a
Cédric Berger <cedric@precidata.com>
2023-07-16 00:25:26 +0200

    if_slnet: simplify mii code

--- rtemsbsd/sys/dev/slnet/if_slnet.c
+++ rtemsbsd/sys/dev/slnet/if_slnet.c
@@ -54,14 +54,10 @@
 
 #include <machine/bus.h>
 
-#include <dev/mii/mii.h>
-#include <dev/mii/miivar.h>
-
-#include <rtems/bsd/local/miibus_if.h>
-
 #include <bios_core.h>
 
-#define MAX_QLEN    64
+#define SLNET_MAX_QLEN    64
+#define SLNET_BMSR_CAPS	(BMSR_10THDX | BMSR_10TFDX | BMSR_100TXHDX | BMSR_100TXFDX /* | BMSR_ANEG */)
 
 #define	SLNET_LOCK(sc) mtx_lock(&(sc)->mtx)
 #define	SLNET_UNLOCK(sc) mtx_unlock(&(sc)->mtx)
@@ -69,10 +65,10 @@
 struct slnet_softc {
     uint8_t		 mac_addr[6];
     struct ifnet	*ifp;
-    device_t		 miibus;
-    struct mii_data	*mii_softc;
+    struct ifmedia	media;		/* Media config (fake). */
     struct mtx		 mtx;
     struct callout	 tick_callout;
+    int			 iid;
 };
 
 static void
@@ -90,14 +86,7 @@ slnet_init(void *arg)
     struct slnet_softc *sc = arg;
     struct ifnet *ifp = sc->ifp;
 
-    mii_mediachg(sc->mii_softc);
     callout_reset(&sc->tick_callout, hz, slnet_tick, sc);
-}
-
-static int
-slnet_transmit(struct ifnet *ifp, struct mbuf *m)
-{
-    return (ENETDOWN);
 }
 
 static void
@@ -106,46 +95,61 @@ slnet_qflush(struct ifnet *ifp)
 }
 
 static int
-slnet_ioctl(struct ifnet *ifp, ioctl_command_t cmd, caddr_t data)
+slnet_transmit(struct ifnet *ifp, struct mbuf *m)
 {
-    return (EINVAL);
-}
-
-static void
-slnet_media_status(struct ifnet *ifp, struct ifmediareq *ifmr_p)
-{
-	struct slnet_softc *sc = ifp->if_softc;
-	struct mii_data *mii;
-
-	SLNET_LOCK(sc);
-	mii = sc->mii_softc;
-	if (mii != NULL) {
-		mii_pollstat(mii);
-		ifmr_p->ifm_active = mii->mii_media_active;
-		ifmr_p->ifm_status = mii->mii_media_status;
-	}
-	SLNET_UNLOCK(sc);
+    return (ENETDOWN);
 }
 
 static int
-slnet_media_change(struct ifnet *ifp)
+slnet_ioctl(struct ifnet *ifp, u_long cmd, caddr_t data)
 {
 	struct slnet_softc *sc = ifp->if_softc;
+	struct ifreq *ifr = (struct ifreq *)data;
 	int error;
 
-	SLNET_LOCK(sc);
-	if (sc->mii_softc != NULL)
-		error = mii_mediachg(sc->mii_softc);
-	else
-		error = ENXIO;
-	SLNET_UNLOCK(sc);
+	switch (cmd) {
+	case SIOCSIFFLAGS:
+	case SIOCADDMULTI:
+	case SIOCDELMULTI:
+		error = 0;
+		break;
+
+	case SIOCSIFMEDIA:
+	case SIOCGIFMEDIA:
+		error = ifmedia_ioctl(ifp, ifr, &sc->media, cmd);
+		break;
+
+	default:
+		/* Let the common ethernet handler process this. */
+		error = ether_ioctl(ifp, cmd, data);
+		break;
+	}
+
 	return (error);
+}
+
+static int
+slnet_media_change(struct ifnet *ifp __unused)
+{
+	/* do nothing */
+	return (0);
+}
+
+static void
+slnet_media_status(struct ifnet *ifp __unused, struct ifmediareq *imr)
+{
+	imr->ifm_status = IFM_AVALID | IFM_ACTIVE;
+	imr->ifm_active = IFM_ETHER | IFM_100_TX | IFM_FDX;
 }
 
 static int
 slnet_probe(device_t dev)
 {
-    device_set_desc(dev, "SL-3011 Virtual ETH");
+    int iid = device_get_unit(dev);
+
+    if (iid < 0 || iid >= BIOS->nveth)
+	return (ENXIO);
+    device_set_desc(dev, "SL-3011 virtual ethernet");
     return (BUS_PROBE_DEFAULT);
 }
 
@@ -154,62 +158,47 @@ slnet_attach(device_t dev)
 {
 	struct slnet_softc *sc;
 	struct ifnet *ifp;
-	int error;
+	int error, caps;
 
 	sc = device_get_softc(dev);
 	sc->ifp = ifp = if_alloc(IFT_ETHER);
+	sc->iid = device_get_unit(dev);
 	ifp->if_softc = sc;
 
 	mtx_init(&sc->mtx, device_get_nameunit(dev), MTX_NETWORK_LOCK, MTX_DEF);
 	callout_init_mtx(&sc->tick_callout, &sc->mtx, 0);
 
-	rtems_bsd_get_mac_address(device_get_name(dev), device_get_unit(dev),
-	    &sc->mac_addr[0]);
+	rtems_bsd_get_mac_address(device_get_name(dev), sc->iid, &sc->mac_addr[0]);
 
-	if_initname(ifp, "ve", device_get_unit(dev));
-	ifp->if_flags = IFF_SIMPLEX | IFF_MULTICAST | IFF_BROADCAST;
-	ifp->if_capenable = ifp->if_capabilities;
+	/* Initialise pseudo media types. */
+	ifmedia_init(&sc->media, 0, slnet_media_change, slnet_media_status);
+	ifmedia_add(&sc->media, IFM_ETHER | IFM_100_TX, 0, NULL);
+	ifmedia_set(&sc->media, IFM_ETHER | IFM_100_TX);
+
+	if_initname(ifp, "veth", sc->iid);
+	ifp->if_dunit = sc->iid;
+	ifp->if_flags = IFF_BROADCAST | IFF_SIMPLEX | IFF_MULTICAST;
+	ifp->if_capabilities = 0; /* IFCAP_VLAN_MTU ? */
+	ifp->if_capenable = 0; /* IFCAP_VLAN_MTU ? */
 	ifp->if_transmit = slnet_transmit;
 	ifp->if_qflush = slnet_qflush;
 	ifp->if_ioctl = slnet_ioctl;
-	ifp->if_init = slnet_init;
-
-	IFQ_SET_MAXLEN(&ifp->if_snd, MAX_QLEN);
-	ifp->if_snd.ifq_drv_maxlen = MAX_QLEN;
-	IFQ_SET_READY(&ifp->if_snd);
-
-	error = mii_attach(dev, &sc->miibus, ifp, slnet_media_change,
-	    slnet_media_status, BMSR_DEFCAPMASK, MII_PHY_ANY,
-	    MII_OFFSET_ANY, 0);
-	if (error == 0)
-		sc->mii_softc = device_get_softc(sc->miibus);
-
+	ifp->if_init  = slnet_init;
+	if_setsendqlen(ifp, ifqmaxlen);
+	if_setsendqready(ifp);
+	ifp->if_baudrate = IF_Mbps(100);
+    
 	ether_ifattach(ifp, &sc->mac_addr[0]);
 
+	ifp->if_drv_flags |= IFF_DRV_RUNNING;
+	if_link_state_change(ifp, LINK_STATE_UP);
+
 	return (0);
-}
-
-static int
-slnet_miibus_read(device_t dev, int phy, int reg)
-{
-	if (phy < 0 || phy >= BIOS->nveth)
-		return (-1);
-	if (reg < 0 || reg >= BIOS_VIRTUAL_MII_NREGS)
-		return (0);
-	return (BIOS->veths[phy].mii->regs[reg]);
-}
-
-static int
-slnet_miibus_write(device_t dev, int phy, int reg, int val)
-{
-	return (-1);
 }
 
 static device_method_t slnet_methods[] = {
 	DEVMETHOD(device_probe, slnet_probe),
 	DEVMETHOD(device_attach, slnet_attach),
-	DEVMETHOD(miibus_readreg, slnet_miibus_read),
-	DEVMETHOD(miibus_writereg, slnet_miibus_write),
 	DEVMETHOD_END
 };
 
@@ -222,9 +211,6 @@ driver_t slnet_driver = {
 static devclass_t slnet_devclass;
 
 DRIVER_MODULE(slnet, nexus, slnet_driver, slnet_devclass, 0, 0);
-DRIVER_MODULE(miibus, slnet, miibus_driver, miibus_devclass, 0, 0);
-
 MODULE_DEPEND(slnet, ether, 1, 1, 1);
-MODULE_DEPEND(slnet, miibus, 1, 1, 1);
 
 #endif /* LIBBSP_ARM_STM32H7_BSP_H */
