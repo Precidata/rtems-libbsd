9697807b6f8088b96df14ad79f956da4f13f5fba
Karel Gardas <karel@functional.vision>
2023-07-30 22:42:01 +0200

    WIP: import quickjs into the contrib/quickjs directory

---
+++ freebsd/contrib/quickjs/quickjscmds-config.h
@@ -0,0 +1,17 @@
+
+#ifndef _RTEMS_QUICKJSCMDS_CONFIG_h
+#define _RTEMS_QUICKJSCMDS_CONFIG_h
+
+#include <rtems/shell.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+extern rtems_shell_cmd_t rtems_shell_QJS_Command;
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif
